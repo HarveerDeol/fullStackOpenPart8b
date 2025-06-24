9d1c88f72b720d3b7109ed7b2d1753223e24758a
Harveer Deol <harveerdeol53@gmail.com>
2025-06-24 09:50:22 -0600

    exercise 8.8

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -1,30 +1,48 @@
+import { gql, useQuery } from '@apollo/client'
+
 const Authors = (props) => {
   if (!props.show) {
     return null
   }
-  const authors = []
 
-  return (
-    <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>born</th>
-            <th>books</th>
+  const query = gql`
+  query {
+    allAuthors {
+      bookCount
+      born
+      name
+      id
+    }
+  }
+`
+const result = useQuery(query)
+
+if (result.loading) {
+  return <div>loading...</div>
+}
+const authors = result.data.allAuthors //was attempting to push result.data.allAuthors on authors 
+
+return (
+  <div>
+    <h2>authors</h2>
+    <table>
+      <tbody>
+        <tr>
+          <th></th>
+          <th>born</th>
+          <th>books</th>
+        </tr>
+        {authors.map((a) => (
+          <tr key={a.id}>
+            <td>{a.name}</td>
+            <td>{a.born}</td>
+            <td>{a.bookCount}</td>
           </tr>
-          {authors.map((a) => (
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+        ))}
+      </tbody>
+    </table>
+  </div>
+)
 }
 
 export default Authors
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+import { ApolloClient, ApolloProvider, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+})
+
+const query = gql`
+  query AllAuthors {
+    allAuthors {
+      bookCount
+      born
+      name
+    }
+  }
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
-);
+  </ApolloProvider>
+)
